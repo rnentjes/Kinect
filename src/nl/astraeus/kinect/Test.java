0c45645ba8c3ca26ec6dc7f34c8905008c2d784e
rnentjes <rien@nentjes.com>
2013-06-08 16:46:27 +0200

    Show skeleton with processing lib

---
+++ src/nl/astraeus/kinect/Test.java
@@ -0,0 +1,11 @@
+package nl.astraeus.kinect;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Gebruiker
+ * Date: 8-6-13
+ * Time: 16:46
+ * To change this template use File | Settings | File Templates.
+ */
+public class Test {
+}
